748897170a8e00a443fe897cf2726acad46769b6
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-12-24 15:40:24 -0300

    Update main.dart

--- fundamentals/first_app/lib/main.dart
+++ fundamentals/first_app/lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor:Color.fromARGB(255, 171, 69, 225) ,
-      body: Center(
-        child: Text('Hello World'),
+      body: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 8,
+          ),
+          borderRadius: BorderRadius.circular(12),
+          gradient: LinearGradient(
+            colors: [Colors.red, Colors.yellow],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+
+        child: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(color: Colors.white, fontSize: 28),
+          ),
+        ),
       ),
+
     ),
   ));
 }
