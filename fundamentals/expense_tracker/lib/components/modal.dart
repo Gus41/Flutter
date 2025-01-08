440ec22737134ea5c27d76fd0d801a64cb57055b
Gustavo <123191793+Gus41@users.noreply.github.com>
2025-01-08 19:31:47 -0300

    Update modal.dart

--- fundamentals/expense_tracker/lib/components/modal.dart
+++ fundamentals/expense_tracker/lib/components/modal.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+final formatter = DateFormat.yMd();
 
 class Modal extends StatefulWidget {
   const Modal({super.key});
@@ -10,16 +12,29 @@ class Modal extends StatefulWidget {
 }
 
 class _Modal extends State<Modal> {
-
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
-
+  DateTime? _date;
 
   @override
   void dispose() {
     _titleController.dispose();
     _amountController.dispose();
     super.dispose();
+  }
+
+  void _showDatePicker() async {
+
+    final date = await showDatePicker(
+      context: context, 
+      firstDate: DateTime.now(), 
+      lastDate: DateTime.now()
+      );
+
+    setState(() {
+      _date = date;
+    });
+
   }
 
   @override
@@ -31,34 +46,52 @@ class _Modal extends State<Modal> {
           TextField(
             maxLength: 50,
             controller: _titleController,
-            decoration: InputDecoration(
-              label: Text("Title")
-            ),
+            decoration: InputDecoration(label: Text("Title")),
           ),
-          TextField(
-            controller: _amountController,
-            decoration: InputDecoration(
-              label: Text("Amount"),
-              prefixText: "R\$ "
-            ),
-            keyboardType: TextInputType.number,
-          ),
-          const SizedBox(height: 10,),
           Row(
             children: [
-              ElevatedButton(onPressed: (){
-                print(_titleController.text);
-                print(_amountController.text);
-                Navigator.pop(context);
-
-              }, child: const Text("Save expense")),
+              Expanded(
+                  child: TextField(
+                controller: _amountController,
+                decoration:
+                    InputDecoration(label: Text("Amount"), prefixText: "R\$ "),
+                keyboardType: TextInputType.number,
+              )),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(_date == null? 'No date selected' : formatter.format(_date!)),
+                      IconButton(
+                          onPressed: _showDatePicker, icon: const Icon(Icons.calendar_month))
+                    ],
+              ))
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Save expense")),
               const Spacer(),
-              TextButton(onPressed: (){
-                Navigator.pop(context);
-              }, child: Text("Cancel"))
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Cancel"))
             ],
           )
-
         ],
       ),
     );
