0752a42c4f9e88220d828fefbe764dcfa04c9eaa
Gustavo <123191793+Gus41@users.noreply.github.com>
2025-01-17 22:51:47 -0300

    recreate project

--- fundamentals/meals/lib/categories.dart
+++ fundamentals/meals/lib/categories.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
-class CategoriesScreen extends StatelessWidget{
-
-  const CategoriesScreen({super.key}); 
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        title: const Text("Pick yout Category"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1.5,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          Text("1", style: TextStyle(color:Colors.white),),
+          Text("1", style: TextStyle(color:Colors.white),),
+        ],
       ),
     );
   }
